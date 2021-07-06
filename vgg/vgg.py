8d79e498d68ac7b11b58d1b76bf11ad0270be027
Davis Yoshida <dyoshida@ttic.edu>
2021-07-06 17:04:58 -0500

    Add option to just return features instead of logits

--- vgg/vgg.py
+++ vgg/vgg.py
@@ -15,17 +15,19 @@ CFG = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 
 class Features(hk.Module):
     def __call__(self, x):
         module_num = 0
+        features = []
         for layer_type in CFG:
             if layer_type == 'M':
                 x = hk.max_pool(x, window_shape=2, strides=2, padding='VALID', channel_axis=0)
                 module_num += 1
+                features.append(x)
             else:
                 conv = hk.Conv2D(layer_type, kernel_shape=3, padding=(1,1), data_format='NCHW', name=f'conv_{module_num}')
                 x = conv(x)
                 module_num += 1
                 x = jax.nn.relu(x)
                 module_num += 1
-        return x
+        return x, features
 
 class Classifier(hk.Module):
     def __call__(self, x):
@@ -42,17 +44,23 @@ class Classifier(hk.Module):
         return x
 
 class VGG16(hk.Module):
-    def __call__(self, x):
+    def __call__(self, x, output_features=False, output_logits=True):
         """x should be in CHW format"""
-        x = Features(name='features')(x)
-        x = adaptive_pool(hk.avg_pool, x, (7, 7))
+        outputs = {}
 
-        x = x.reshape(-1)
+        x, features = Features(name='features')(x)
+        if output_features:
+            outputs['features'] = features
 
-        x = Classifier(name='classifier')(x)
+        if output_logits:
+            x = adaptive_pool(hk.avg_pool, x, (7, 7))
 
-        return x
+            x = x.reshape(-1)
 
+            x = Classifier(name='classifier')(x)
+            outputs['logits'] = x
+
+        return outputs
 
 def _dicts_to_flatmappings(tree):
     if isinstance(tree, dict):
@@ -67,8 +75,8 @@ def get_model(with_dropout=False, weights_location='weights.pkl'):
     weights = jax.tree_map(jnp.array, weights)
     weights = _dicts_to_flatmappings(weights)
 
-    def model_fn(x):
-        return VGG16(name='model')(x)
+    def model_fn(x, output_features=False, output_logits=True):
+        return VGG16(name='model')(x, output_features=output_features, output_logits=output_logits)
 
     model = hk.transform(model_fn)
     if not with_dropout:
